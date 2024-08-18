493c42182433edf9efc1eb2cbaf57287bd5e61df
mikesvr6 <michaeloceallachain@gmail.com>
2024-08-18 15:33:43 +0000

    commit 3 errors resolved for mbed 6, mbed os error in trminal now

--- main.cpp
+++ main.cpp
@@ -15,32 +15,42 @@ Location: 0x4353 Error Value: 0xFFFFFFFF
 
 
 DigitalOut myled(LED1);
-Serial pc(USBRX, USBTX); // This script appears to be for a previous version of mbed, to fix we wil have to change the coding for the mbed 6 version aswell as any other errors 
-Serial dev(p9, p10); // Pin nomenclature is incorrect as use of "Serial" in mbed 6
+UnbufferedSerial pc(USBRX, USBTX); /* This script appears to be for a previous version of mbed, 
+to fix we wil have to change the coding for the mbed 6 version aswell as any other errors 
+unbufferredSerial added above, USBRX and TX need to swapped for proper order*/
+
+UnbufferedSerial dev(PA_9, PA_10); /* Pin nomenclature is incorrect as use of "Serial" in mbed 6. 
+UnbufferedSerial added here. Pins changed to proper format, issue may not be solved!!   */
+
+const char* buffer; // buffer variable added for data storage
+
 
 void dev_recv(){
     while(dev.readable()){
-        pc.putc(dev.getc())    // putc and getc are not used in mbed 6, will need to add a colon here as well
+        dev.read(&buffer, 8);   
+        pc.write(&buffer, 8); 
+    // putc and getc are not used in mbed 6, will need to add a colon here as well. get and put replaced with read and write
     }    
 }
 
 void pc_recv(){
     while(pc.readable()){
-        dev.putc(pc.getc());    // as above
+        pc.read(&buffer, 8);
+        dev.write(&buffer, 8);       // as above
     }    
 }
 
 int main() {
     pc.baud(9600);
-    device1.baud(115200); //device1 is not correct here nd will need to be changed to dev as well as the baud rate from 115200 to 9600
+    dev.baud(9600); //device1 is not correct here nd will need to be changed to dev as well as the baud rate from 115200 to 9600
     pc.attach(&pc_recv);
-    dev.atach(&dev); //there is no dev interrupt , needs to be changed to &dev_recv
-    pc.printf("Hello!! \r\n"); // printf here wiill be an issue
+    dev.attach(&dev_recv); //there is no dev interrupt , needs to be changed to &dev_recv
+    pc.write("Hello!! \r\n", 8); // printf here will be an issue, replaced with write
     
     while(1) {
         myled = 1;
-        wait(1); // wait here is incorrect and not in use. will need to be changed
+        ThisThread::sleep_for(1s); // wait here is incorrect and not in use. will need to be changed, replaced with sleep_for
         myled = 0;
-        wait(1); // as above
+        ThisThread::sleep_for(1s); // as above
     }
 }
